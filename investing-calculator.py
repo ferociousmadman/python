092c11a2fd62a6e66b39a00755258e9a450a2158
Skipper Davies <48104082+ferociousmadman@users.noreply.github.com>
2020-04-22 22:07:06 -0400

    Update investing-calculator.py
    Revision to the original investing-calculator. Fills out one of the previously unfilled functions and corrects various errors in the program that were triggering bad results.

--- investing-calculator.py
+++ investing-calculator.py
@@ -15,19 +15,45 @@ import sys
 
 
 def main():
-    usr_input = 11
-    while usr_input != 0:
-        usr_input = input('Please enter a number to select from the following options:\n0 To end program\n1 Calculate your personal cash flow\n2 Calculate the amount of debt you are in vs income\n3 Calculate total return on an investment\n4 Calculate simple return on an investment\n5 Calculate compounding annual growth for an investment\n6 Calculate company earnings yield\n7 Calculate company return on capital\n8 Calculate inflation adjusted ROI\n9 Calculate gains or losses\n10 Calculate how long it will take for you to double your money on an investment\n')
+    continue_or_end = '3'
+    options_for_response = [end_program, personal_cashflow, personal_leverage_ratio, total_return, simple_return,
+                            compound_annual_growth, company_earnings_yield, company_return_on_capital,
+                            inflation_adjusted_roi, calculate_gains_losses, how_long_to_double_money]
+    while continue_or_end != '0':
+        continue_or_end = input('Enter 0 to exit or 1 for option selections: ')
+        if continue_or_end == '1':
+            usr_input = input('''\nPlease enter a number to select from the following options:
+0 To end program
+1 Calculate your personal cash flow
+2 Calculate the amount of debt you are in vs income
+3 Calculate total return on an investment
+4 Calculate simple return on an investment
+5 Calculate compounding annual growth for an investment
+6 Calculate company earnings yield
+7 Calculate company return on capital
+8 Calculate inflation adjusted ROI
+9 Calculate gains or losses
+10 Calculate how long it will take for you to double your money on an investment\n''')
+        elif continue_or_end == '0':
+            sys.exit(0)
+        else:
+            continue
         usr_input = error_check(usr_input)
 
-        options_for_response = [end_program(), personal_cashflow(usr_input), personal_leverage_ratio(usr_input), total_return(), simple_return(), compound_annual_growth(), company_earnings_yield(), company_return_on_capital(), inflation_adjusted_roi(), calculate_gains_losses(), how_long_to_double_money()]
-        for i in options_for_response:
-            if options_for_response[i] == usr_input:
-                return options_for_response[i]
+        options_for_response[usr_input]()
 
 
 def total_return():
-    pass
+    value_of_investment_end_of_year = input(
+        'Enter the value of investment at end of the year (no dollar signs or commas): ')
+    value_of_investment_end_of_year = error_check_accounting(value_of_investment_end_of_year)
+    value_of_investment_start_of_year = input(
+        'Enter the value of investment at beginning of the year (no dollar signs or commas): ')
+    value_of_investment_start_of_year = error_check_accounting(value_of_investment_start_of_year)
+    dividends = input('Enter the value of total dividends for the year: ')
+    dividends = error_check_accounting(dividends)
+    result = ((value_of_investment_end_of_year - value_of_investment_start_of_year) + dividends) / value_of_investment_start_of_year
+    print('You have received a ' + '{:.1%}'.format(result) + ' percent return on your investment.')
 
 
 def simple_return():
@@ -46,29 +72,28 @@ def company_return_on_capital():
     pass
 
 
-def personal_cashflow(usr_input):
+def personal_cashflow():
     monthly_personal_income = input('Enter your total personal income per month: ')
     monthly_personal_income = error_check_accounting(monthly_personal_income)
     monthly_personal_expenses = input('Enter your total personal expenses per month: ')
     monthly_personal_expenses = error_check_accounting(monthly_personal_expenses)
     cashflow = monthly_personal_income - monthly_personal_expenses
-    print('Calculating cash flow helps you understand your true living expenses.\nNegative cash flow means you are spending more than you are bringing in.\nPositive cash flow means you are living within your means.\nYour cash flow is:', cashflow)
-    while usr_input != 0:
-        usr_input = input('\nPlease enter a number to select from the following options:\n0 To end program\n1 Calculate your personal cash flow\n2 Calculate the amount of debt you are in vs income\n3 Calculate total return on an investment\n4 Calculate simple return on an investment\n5 Calculate compounding annual growth for an investment\n6 Calculate company earnings yield\n7 Calculate company return on capital\n8 Calculate inflation adjusted ROI\n9 Calculate gains or losses\n10 Calculate how long it will take for you to double your money on an investment\n')
-        usr_input = error_check(usr_input)
+    print(
+        '''Calculating cash flow helps you understand your true living expenses.
+Negative cash flow means you are spending more than you are bringing in.
+Positive cash flow means you are living within your means.
+Your cash flow is:''',
+        cashflow)
 
 
-def personal_leverage_ratio(usr_input):
-
+def personal_leverage_ratio():
     monthly_debt_payments = input('Enter the total payment you make on all debt per month: ')
     monthly_debt_payments = error_check_accounting(monthly_debt_payments)
     monthly_personal_income = input('Enter your total personal income per month: ')
     monthly_personal_income = error_check_accounting(monthly_personal_income)
     leverage_ratio = monthly_debt_payments / monthly_personal_income
-    print('A common standard is that your debt ratio should be no more than 33% (.33) of your income. Yours is:', leverage_ratio)
-    while usr_input != 0:
-        usr_input = input('\nPlease enter a number to select from the following options:\n0 To end program\n1 Calculate your personal cashflow\n2 Calculate the amount of debt you are in vs income\n3 Calculate total return on an investment\n4 Calculate simple return on an investment\n5 Calculate compounding annual growth for an investment\n6 Calculate company earnings yield\n7 Calculate company return on capital\n8 Calculate inflation adjusted ROI\n9 Calculate gains or losses\n10 Calculate how long it will take for you to double your money on an investment\n')
-        usr_input = error_check(usr_input)
+    print('A common standard is that your debt ratio should be no more than 33% (.33) of your income. Yours is:',
+          leverage_ratio)
 
 
 def inflation_adjusted_roi():
@@ -93,10 +118,32 @@ def error_check(usr_input):
                 sys.exit(0)
             else:
                 print('ERROR: please enter a valid positive whole number 0 - 10')
-                usr_input = input('Please enter a number to select from the following options:\n1 Calculate your personal cashflow\n2 Calculate the amount of debt you are in vs income\n3 Calculate total return on an investment\n4 Calculate simple return on an investment\n5 Calculate compounding annual growth for an investment\n6 Calculate company earnings yield\n7 Calculate company return on capital\n8 Calculate inflation adjusted ROI\n9 Calculate gains or losses\n10 Calculate how long it will take for you to double your money on an investment\n')
+                usr_input = input(
+                    '''Please enter a number to select from the following options:'
+1 Calculate your personal cashflow
+2 Calculate the amount of debt you are in vs income
+3 Calculate total return on an investment
+4 Calculate simple return on an investment
+5 Calculate compounding annual growth for an investment
+6 Calculate company earnings yield
+7 Calculate company return on capital
+8 Calculate inflation adjusted ROI
+9 Calculate gains or losses
+10 Calculate how long it will take for you to double your money on an investment\n''')
         except ValueError:
             print('ERROR: please enter a valid positive whole number 0 - 10')
-            usr_input = input('Please enter a number to select from the following options:\n1 Calculate your personal cashflow\n2 Calculate the amount of debt you are in vs income\n3 Calculate total return on an investment\n4 Calculate simple return on an investment\n5 Calculate compounding annual growth for an investment\n6 Calculate company earnings yield\n7 Calculate company return on capital\n8 Calculate inflation adjusted ROI\n9 Calculate gains or losses\n10 Calculate how long it will take for you to double your money on an investment\n')
+            usr_input = input(
+                '''Please enter a number to select from the following options:
+1 Calculate your personal cashflow
+2 Calculate the amount of debt you are in vs income
+3 Calculate total return on an investment
+4 Calculate simple return on an investment
+5 Calculate compounding annual growth for an investment
+6 Calculate company earnings yield
+7 Calculate company return on capital
+8 Calculate inflation adjusted ROI
+9 Calculate gains or losses
+10 Calculate how long it will take for you to double your money on an investment\n''')
     return usr_input
 
 
@@ -115,4 +162,3 @@ def end_program():
 
 
 main()
-
