a5255ca18a6648fc8c49d862d931184a76f5d2bf
Skipper Davies <48104082+ferociousmadman@users.noreply.github.com>
2020-05-04 12:14:41 -0400

    Update jackpot-program.py
    Updated to include basic error handling in the num_error_handler() function. Also breaks main part of the program out into its own function called main().

--- jackpot-payout/jackpot-program.py
+++ jackpot-payout/jackpot-program.py
@@ -7,12 +7,26 @@
 # cash payout. 
 # -----------------------------------------------------------------
 
-jackpot_amount = float(input('Input jackpot amount (do not enter non-numerical characters aside from a decimal point if required): '))
-before_tax_installments = (jackpot_amount / 20)
-after_tax_installments = (jackpot_amount * .70) / 20
-print('Before tax installments amount:', format(before_tax_installments,".2f"))
-print('After tax installments amount:', format(after_tax_installments,".2f"))
-before_tax_cash = (jackpot_amount * .65)
-after_tax_cash = (jackpot_amount * .65) * .70
-print('Before tax cash option amount:', format(before_tax_cash,".2f"))
-print('After tax cash option amount:', format(after_tax_cash,".2f"))
+def main():
+    jackpot_amount = input('Input jackpot amount (do not enter non-numerical characters aside from a decimal point if required): ')
+    jackpot_amount = num_error_handler(jackpot_amount)
+    before_tax_installments = (jackpot_amount / 20)
+    after_tax_installments = (jackpot_amount * .70) / 20
+    print('Before tax installments amount:', format(before_tax_installments,".2f"))
+    print('After tax installments amount:', format(after_tax_installments,".2f"))
+    before_tax_cash = (jackpot_amount * .65)
+    after_tax_cash = (jackpot_amount * .65) * .70
+    print('Before tax cash option amount:', format(before_tax_cash,".2f"))
+    print('After tax cash option amount:', format(after_tax_cash,".2f"))
+
+def num_error_handler(num):
+    while not num.isdecimal():
+        num = input('ERROR (must enter an integer or floating point number): ')
+        try:
+            num = float(num)
+            break
+        except ValueError:
+            continue
+    return int(num)
+
+main()
